935e556a9196a9815b5c9124c3b32799bd91f45c
Meeka <rrgayhart@gmail.com>
2014-03-18 19:17:42 -0600

    adds pg connection but punts offical db creation

--- server.js
+++ server.js
@@ -12,3 +12,13 @@ var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+//var pg = require('pg');
+
+//pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//  client.query('SELECT * FROM your_table', function(err, result) {
+//    done();
+//    if(err) return console.error(err);
+//    console.log(result.rows);
+//  });
+// });
